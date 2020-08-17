2badb9d94e2df636695c15ae020cf711e716fb54
gucao <280780363@qq.com>
2020-08-17 11:05:03 +0800

    ...

--- src/Shashlik.Kernel/Autowire/Attributes/AutoOptionsAttribute.cs
+++ src/Shashlik.Kernel/Autowire/Attributes/AutoOptionsAttribute.cs
@@ -8,6 +8,16 @@ namespace Shashlik.Kernel.Autowire.Attributes
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class AutoOptionsAttribute : Attribute
     {
+        public AutoOptionsAttribute(string section)
+        {
+            if (string.IsNullOrWhiteSpace(section))
+            {
+                throw new ArgumentException($"“{nameof(section)}”不能为 Null 或空白", nameof(section));
+            }
+
+            Section = section;
+        }
+
         /// <summary>
         /// 配置节点名称，例：Logger:Microsft
         /// </summary>
