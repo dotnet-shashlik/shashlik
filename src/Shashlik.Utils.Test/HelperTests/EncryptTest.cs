f462db31feda907d77b491f0dd46b97d1b439b51
gucao <280780363@qq.com>
2020-09-19 10:16:29 +0800

    add TripleDesHelper

--- src/Shashlik.Utils.Test/HelperTests/EncryptTest.cs
+++ src/Shashlik.Utils.Test/HelperTests/EncryptTest.cs
@@ -1,16 +1,24 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using Shashlik.Utils.Helpers.Encrypt;
 using Shouldly;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Shashlik.Utils.Test.HelperTests
 {
     public class EncryptTest
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public EncryptTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void DesEncryptTest()
         {
-
             var key = "12345678";
             var iv = "12345678";
             var data = DesHelper.Encrypt("DES加密", key, iv);
@@ -52,6 +60,20 @@ namespace Shashlik.Utils.Test.HelperTests
             var hash = BCryptHelper.Hash(password);
             var result = BCryptHelper.Verify(password, hash);
             result.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void TripleDesTest()
+        {
+            var str = "123123";
+            var password = "698ac4d6b9be40a180d65849";
+            password.Length.ShouldBe(24);
+            _testOutputHelper.WriteLine(password);
+
+            var encoded = TripleDesHelper.Encrypt(str, password);
+            _testOutputHelper.WriteLine(encoded);
+            var decoded = TripleDesHelper.Decrypt(encoded, password);
+            decoded.ShouldBe(str);
         }
     }
 }
---
+++ src/Shashlik.Utils/Helpers/Encrypt/TripleDesHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Shashlik.Utils.Extensions;
+
+namespace Shashlik.Utils.Helpers.Encrypt
+{
+    /// <summary>
+    /// 3desc
+    /// </summary>
+    public class TripleDesHelper
+    {
+        public static string Encrypt(string plainText, string key, CipherMode mode = CipherMode.ECB,
+            PaddingMode paddingMode = PaddingMode.PKCS7, Encoding encoder = null, string iv = null)
+        {
+            encoder ??= Encoding.UTF8;
+            using var des = CreateDes(key);
+            using var ct = des.CreateEncryptor();
+            var input = encoder.GetBytes(plainText);
+            var output = ct.TransformFinalBlock(input, 0, input.Length);
+            return Convert.ToBase64String(output);
+        }
+
+        public static string Decrypt(string cypherText, string key, CipherMode mode = CipherMode.ECB,
+            PaddingMode paddingMode = PaddingMode.PKCS7, Encoding encoder = null,
+            string iv = null)
+        {
+            encoder ??= Encoding.UTF8;
+            using var des = CreateDes(key);
+            using var ct = des.CreateDecryptor();
+            var input = Convert.FromBase64String(cypherText);
+            var output = ct.TransformFinalBlock(input, 0, input.Length);
+            return encoder.GetString(output);
+        }
+
+        private static TripleDES CreateDes(string key, CipherMode mode = CipherMode.ECB,
+            PaddingMode paddingMode = PaddingMode.PKCS7, string iv = null)
+        {
+            TripleDES des = new TripleDESCryptoServiceProvider();
+            var desKey = Encoding.ASCII.GetBytes(key);
+            des.Key = desKey;
+            if (!iv.IsNullOrWhiteSpace())
+                des.IV = Encoding.ASCII.GetBytes(iv);
+            des.Padding = paddingMode;
+            des.Mode = mode;
+            return des;
+        }
+    }
+}
