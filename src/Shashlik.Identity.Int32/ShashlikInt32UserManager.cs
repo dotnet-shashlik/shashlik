c06b700d14384db6f8a6ebbd4bb7f8843bf4d8c7
gucao <280780363@qq.com>
2020-10-27 17:18:43 +0800

    add ShashlikInt32UserManager

---
+++ src/Shashlik.Identity.Int32/ShashlikInt32UserManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+// ReSharper disable CheckNamespace
+
+namespace Shashlik.Identity
+{
+    public class ShashlikInt32UserManager : ShashlikUserManager<Users, int>
+    {
+        public ShashlikInt32UserManager(IUserStore<Users> store, IOptions<IdentityOptions> optionsAccessor,
+            IPasswordHasher<Users> passwordHasher, IEnumerable<IUserValidator<Users>> userValidators,
+            IEnumerable<IPasswordValidator<Users>> passwordValidators, ILookupNormalizer keyNormalizer,
+            IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<Users>> logger) : base(store,
+            optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services,
+            logger)
+        {
+        }
+    }
+}
