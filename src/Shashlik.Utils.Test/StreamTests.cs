c81c59c0594d12f10bb8c64c9ac44677f609c4ef
zhzhwcn <zhzhwmail@gmail.com>
2020-10-27 19:47:14 +0800

    add StreamTests

---
+++ src/Shashlik.Utils.Test/StreamTests.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Shashlik.Utils.Extensions;
+using Shashlik.Utils.Helpers;
+using Shouldly;
+using Xunit;
+
+namespace Shashlik.Utils.Test
+{
+    public class StreamTests
+    {
+        [Fact]
+        public void Tests()
+        {
+            var str = "testStr中文";
+            var bytes = Encoding.UTF8.GetBytes(str);
+            using (var ms = new MemoryStream(bytes))
+            {
+                ms.GetMD5Hash().ShouldBe(HashHelper.MD5(str));
+            }
+
+            using (var ms = new MemoryStream(bytes))
+            {
+                ms.Seek(3, SeekOrigin.Begin);
+                ms.ReadToString(Encoding.UTF8).ShouldBe(str);
+            }
+
+            using (var ms = new MemoryStream(bytes))
+            {
+                ms.Seek(3, SeekOrigin.Begin);
+                ms.ReadAll().ShouldBe(bytes);
+            }
+        }
+
+        [Fact]
+        public async Task AsyncTests()
+        {
+            var str = "testStr中文";
+            var bytes = Encoding.UTF8.GetBytes(str);
+
+            using (var ms = new MemoryStream(bytes))
+            {
+                ms.Seek(3, SeekOrigin.Begin);
+                (await ms.ReadToStringAsync(Encoding.UTF8)).ShouldBe(str);
+            }
+
+            using (var ms = new MemoryStream(bytes))
+            {
+                ms.Seek(3, SeekOrigin.Begin);
+                (await ms.ReadAllAsync()).ShouldBe(bytes);
+            }
+        }
+    }
+}
