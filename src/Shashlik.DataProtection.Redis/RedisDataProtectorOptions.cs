e9180502309b11ca15d617c32b21efb19d2de7d4
gucao <280780363@qq.com>
2020-11-16 15:33:40 +0800

    add Shashlik.DataProtection.Redis.Tests

--- src/Shashlik.DataProtection.Redis/RedisDataProtectorOptions.cs
+++ src/Shashlik.DataProtection.Redis/RedisDataProtectorOptions.cs
@@ -1,4 +1,5 @@
 ﻿using Shashlik.Kernel.Attributes;
+
 // ReSharper disable CheckNamespace
 
 namespace Shashlik.DataProtection
@@ -20,10 +21,5 @@ namespace Shashlik.DataProtection
         /// 应用程序名称
         /// </summary>
         public string ApplicationName { get; set; } = "DefaultApplicationName";
-
-        /// <summary>
-        /// 全局token生命周期,单位分,默认2小时(官方默认为1天).如需对不同的provider进行过期设置见:https://docs.microsoft.com/zh-cn/aspnet/core/security/authentication/accconfirm?view=aspnetcore-3.1&tabs=visual-studio
-        /// </summary>
-        public int TokenLifespan { get; set; } = 120;
     }
 }
