030817480135dc27a7b0f68c62a5b504d9f8ec45
gucao <280780363@qq.com>
2020-11-16 18:21:59 +0800

    add AddPrefixFormatter&AddSuffixFormatter

---
+++ src/Shashlik.RazorFormat/IFormatter.AddPrefix.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+using System;
+using Shashlik.Utils.Extensions;
+
+namespace Shashlik.RazorFormat
+{
+    /**
+     * 
+     * addPrefix格式化器
+     * e.g.  value = "abc"
+     * addPrefix(demo:) -> "demo:abc"
+     */
+    /// <summary>
+    /// switch格式化器
+    /// </summary>
+    public class AddPrefixFormatter : IFormatter
+    {
+        public string Action => "addPrefix";
+
+        public string? Format(string? value, string expression)
+        {
+            return $"{expression}{value}";
+        }
+    }
+}
---
+++ src/Shashlik.RazorFormat/IFormatter.AddSuffix.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+using System;
+using Shashlik.Utils.Extensions;
+
+namespace Shashlik.RazorFormat
+{
+    /**
+     * 
+     * addSuffix格式化器
+     * e.g.  value = "abc"
+     * addSuffix(...) -> "abc..."
+     */
+    /// <summary>
+    /// switch格式化器
+    /// </summary>
+    public class AddSuffixFormatter : IFormatter
+    {
+        public string Action => "addSuffix";
+
+        public string? Format(string? value, string expression)
+        {
+            return $"{expression}{value}";
+        }
+    }
+}
