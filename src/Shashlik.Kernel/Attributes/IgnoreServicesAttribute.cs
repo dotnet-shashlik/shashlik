616983fa8c8580f3e50e189f03c958638fdb41b5
gucao <280780363@qq.com>
2020-11-16 20:04:08 +0800

    add ExternalServicesAttribute&IgnoreServicesAttribute

---
+++ src/Shashlik.Kernel/Attributes/IgnoreServicesAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Shashlik.Kernel.Attributes
+{
+    public class IgnoreServicesAttribute : Attribute
+    {
+        /// <summary>
+        /// 将忽略的服务
+        /// </summary>
+        /// <param name="ignoreServices"></param>
+        public IgnoreServicesAttribute(params Type[] ignoreServices)
+        {
+            IgnoreServices = ignoreServices;
+        }
+
+        public Type[] IgnoreServices { get; }
+    }
+}
